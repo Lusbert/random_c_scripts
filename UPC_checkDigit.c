3e77000dbebb1c9dbd6d1c128f7e2d3e800e9973
Lusbert <mazinany.armin@gmail.com>
2024-09-19 12:55:05 +0330

    i spent like 30 minutes figuring out why 1-45 was returning -44, yes im dumb

--- UPC_checkDigit.c
+++ UPC_checkDigit.c
@@ -2,16 +2,22 @@
 /*
 program to calculate the check digit on those barcode things on stuff you 
 examples
+    0 13800 15173 5
     0 51500 24128 ?
     0 31200 01005 ?
 the ? is the check digit that this program is supposed to calculate
 */
 int main(void) {
+    int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11;
     printf("Enter the first (single) digit: ");
-    printf("\nEnter first group of five digits: ");
-    printf("\nEnter second group of five digits: ");
-    printf("\nCheck Digit: ", checkDigit);
-
+    scanf("%1d", &n1);
+    printf("Enter first group of five digits:  ");
+    scanf("%1d%1d%1d%1d%1d", &n2,&n3,&n4,&n5,&n6);
+    printf("Enter second group of five digits: ");
+    scanf("%1d%1d%1d%1d%1d", &n7,&n8,&n9,&n10,&n11);
+    int sum = (3 * (n1+n3+n5+n7+n9+n11)) + (n2+n4+n6+n8+n10);
+    int checkDigit = 9 - ((sum)-1)%10;
+    printf("Check Digit: %d\n", checkDigit);
     return 0;
 }
 
