7d5aaf9e8feb872d15d0fbce76736dac44bba866
Lusbert <mazinany.armin@gmail.com>
2024-09-20 22:33:21 +0330

    will i even learn C in enough time

--- UPC_checkDigit.c
+++ UPC_checkDigit.c
@@ -15,7 +15,7 @@ int main(void) {
     printf("Enter second group of five digits: ");
     scanf("%1d%1d%1d%1d%1d", &n7,&n8,&n9,&n10,&n11);
     int sum = (3 * (n1+n3+n5+n7+n9+n11)) + (n2+n4+n6+n8+n10);
-    int checkDigit = 9 - ((sum)-1)%10;
+    int checkDigit = 9 - ((sum-1)%10);
     printf("Check Digit: %d\n", checkDigit);
     return 0;
 }
