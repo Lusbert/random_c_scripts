5df01e6538da128ff0a5a43eb182ee8c9eabab6a
Lusbert <mazinany.armin@gmail.com>
2024-09-21 19:51:56 +0330

    made upc check digit only ask once instead of 3 times

--- UPC_checkDigit.c
+++ UPC_checkDigit.c
@@ -8,12 +8,8 @@ the ? is the check digit that this program is supposed to calculate
 */
 int main(void) {
     int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11;
-    printf("Enter the first (single) digit: ");
-    scanf("%1d", &n1);
-    printf("Enter first group of five digits:  ");
-    scanf("%1d%1d%1d%1d%1d", &n2,&n3,&n4,&n5,&n6);
-    printf("Enter second group of five digits: ");
-    scanf("%1d%1d%1d%1d%1d", &n7,&n8,&n9,&n10,&n11);
+    printf("Enter the first 11 digits of a UPC: ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10,&n11);
     int sum = (3 * (n1+n3+n5+n7+n9+n11)) + (n2+n4+n6+n8+n10);
     int checkDigit = 9 - ((sum-1)%10);
     printf("Check Digit: %d\n", checkDigit);
